f8d4a5f6904f3c90aabf0ee49c688631fc6ae343
Antoine07 <antoine.lucsko@gmail.com>
2022-02-24 08:47:07 +0100

    correction exercice

---
+++ LARAVEL/blog/database/seeders/AuthorSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use Illuminate\Database\Seeder;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $author = new Author();
+        $author->name = "Tarek";
+        $author->email = "tarek@tarek.com";
+
+        $author->save();
+
+        $author = new Author();
+        $author->name = "Michel";
+        $author->email = "michel@michel.com";
+
+        $author->save();
+
+        $author = new Author();
+        $author->name = "Kevin";
+        $author->email = "kevin@kevin.com";
+
+        $author->save();
+
+        $author = new Author();
+        $author->name = "Philippe";
+        $author->email = "philippe@philippe.com";
+
+        $author->save();
+    }
+}
