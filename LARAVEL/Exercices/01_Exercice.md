5e60b4f16c5e98769ec9ea4e013ad9087527a457
Antoine07 <antoine.lucsko@gmail.com>
2022-02-25 09:13:13 +0100

    factory

--- LARAVEL/Exercices/01_Exercice.md
+++ LARAVEL/Exercices/01_Exercice.md
@@ -9,3 +9,15 @@
 ## 02 Exercice 
 
 - Créez un troisième livre sur MongoDB puis associez ce livre à son genre. Terminez les associations de tous les livres créent.
+
+## 02 Exercice faker
+
+Créez un faker pour les auteurs à l'aide de la commande suivante 
+
+```php
+php artisan make:factory AuthorFactory
+```
+
+Puis dans le seeder AuthorSeeder créez 10 auteurs. 
+
+Indications : regardez dans la correction dans BookSeeder et BookFactory comment on a mis en place les factories pour ces entités.
--- LARAVEL/blog/database/seeders/BookSeeder.php
+++ LARAVEL/blog/database/seeders/BookSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Book;
 use App\Models\Genre;
+use Database\Factories\BookFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -30,30 +31,44 @@ class BookSeeder extends Seeder
         // $book  
         // print_r(Genre::all());
 
-        $genrePhp = Genre::where('name', 'php')->firstOrFail();
+        // $genrePhp = Genre::where('name', 'php')->firstOrFail();
 
-        $book = new Book();
+        // $book = new Book();
 
-        $book->title = "PHP avancé";
-        $book->description = Str::random(1000);
-        $book->published_at = new \DateTime;
+        // $book->title = "PHP avancé";
+        // $book->description = Str::random(1000);
+        // $book->published_at = new \DateTime;
 
-        // associer le livre au genre php
-        $book->genre()->associate($genrePhp);
+        // // associer le livre au genre php
+        // $book->genre()->associate($genrePhp);
 
-        $book->save();
+        // $book->save();
 
-        $book = new Book();
+        // $book = new Book();
 
-        $book->title = "Programmer avec MySQL";
-        $book->description = Str::random(1000);
-        $book->published_at = new \DateTime;
+        // $book->title = "Programmer avec MySQL";
+        // $book->description = Str::random(1000);
+        // $book->published_at = new \DateTime;
 
-        $book->save();
+        // $book->save();
 
-        $genres = Genre::all();
+        // $genres = Genre::all();
 
-        // dump une fonction de Laravel pour debug
-        dump($genres);
+        // // dump une fonction de Laravel pour debug
+        // dump($genres);
+
+        // foreach(Genre::all() as $genre){
+        //     $book = new Book ; // crée un book
+        //     $book->title = "titre " . $genre->name; // on set un titre
+        //     $book->description = Str::random(1000); // une description
+        //     $book->published_at = new \DateTime; // on lui met une date
+
+        //     $book->genre()->associate($genre); // associe au genre
+        //     // dump($genre);
+        //     $book->save(); // sauvegarde => créer en base de données
+        // }
+
+        // Factory 
+        Book::factory()->count(50)->create();
     }
 }
