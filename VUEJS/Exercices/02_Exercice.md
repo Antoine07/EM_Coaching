d426178290166d16832054698d5b828acb9b22b7
Antoine07 <antoine.lucsko@gmail.com>
2022-03-22 13:46:43 +0100

    vuejs

---
+++ VUEJS/Exercices/02_Exercice.md
@@ -0,0 +1,13 @@
+# 02 Exercices
+
+1. Affichez la liste de nombres dans le DOM avec Vuejs
+
+2. Affichez uniqument les nombres pairs.
+
+3. Ordonnez cette liste
+
+```js
+const numbers = [19,0,189, 11, 15, 17];
+```
+
+Rmq : faites cette exercice en utilisant les deux fichiers classiques : index.html et app-component.js
--- VUEJS/Exercices/Corrections/01_Exercice_students/app-component.js
+++ VUEJS/Exercices/Corrections/01_Exercice_students/app-component.js
@@ -1,16 +1,39 @@
 // app-component.js
+
 export default {
+
+  // attache une méthode qui peut être appelée dans le rendu 
+  methods: {
+    getNote(id, grades) {
+      return grades.find(grade => grade.id == id ).note
+    }
+  },
+
+  // la méthode qui permet de définir les données
   data() {
+
     return {
       // variable title 
       title: "Liste des étudiants",
-      students: [{ name: 'Alan', age : 45 }, { name: 'Sophie', age : 43 }]
+      grades: [
+        { id: 1, note: 16 },
+        { id: 2, note: 15 },
+      ],
+      students: [
+        { id: 1, name: 'Alan', age: 45 },
+        { id: 2, name: 'Sophie', age: 43 }
+      ]
     }
   },
+  // l'attribut qui permet de monter dans le DOM le rendu 
   template: `
     <h1> {{ title }}</h1>
     <li v-for="student in students">
-      Name : {{ student.name }}
+      Name : {{ student.name }} age : {{ student.age }},
+      Note : {{ getNote(student.id, grades) }}
+    </li>
+    <li v-for="grade in grades">
+      note : {{ grade.note }}
     </li>
     `
 }
