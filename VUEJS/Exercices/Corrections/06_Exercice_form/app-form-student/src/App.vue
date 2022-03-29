befd295fd5f1fed315419cd92ee8caaf46769892
Antoine07 <antoine.lucsko@gmail.com>
2022-03-29 15:19:20 +0200

    form add emit

--- VUEJS/Exercices/Corrections/06_Exercice_form/app-form-student/src/App.vue
+++ VUEJS/Exercices/Corrections/06_Exercice_form/app-form-student/src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
 import AddStudent from './components/AddStudent.vue'
+import { ref } from 'vue'
+const students  = ref([])
 
+function addStudent(e){
+  console.log(e)
+
+  students.value.push( e )
+
+}
 </script>
 
 <template>
-  <AddStudent />
+  <AddStudent @addStudent="addStudent" />
+  <ul v-if="students.length > 0">
+    <li v-for="st in students "> Name: {{st.name}} Age: {{ st.age }} </li>
+  </ul>
 </template>
 
 <style>
--- VUEJS/Exercices/Corrections/06_Exercice_form/app-form-student/src/components/AddStudent.vue
+++ VUEJS/Exercices/Corrections/06_Exercice_form/app-form-student/src/components/AddStudent.vue
@@ -1,10 +1,23 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref, defineEmits } from 'vue'
 const name = ref('')
+const age = ref('')
+const emit = defineEmits(['addStudent'])
+
+function checkForm(e){
+	e.preventDefault()
+
+	emit('addStudent', {name : name.value, age : age.value} )
+
+	name.value = ''
+	age.value = ''
+}
 </script>
 
 <template>
-  <p>Name is: {{ name }}</p>
-	<input v-model="name" placeholder="edit me" />
+<form @submit="checkForm" >
+	<input v-model="name" placeholder="name..." required />
+	<input v-model="age" placeholder="age..." required  />
+	<button>Ok</button>
+</form>
 </template>
