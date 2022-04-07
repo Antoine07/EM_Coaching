c7e116f4adae52b5a217370b7cd370d08c735a31
Antoine07 <antoine.lucsko@gmail.com>
2022-04-07 13:34:26 +0200

    exercices

--- PHP/DM_03/02_Exercice.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$numbers = [1,9,10, 7, 2 ];
-
-// Trouvez à l'aide d'un script l'élément le plus grand du tableau
---
+++ PHP/DM_03/03_Exercice_plus_long.php
@@ -0,0 +1,6 @@
+<?php
+
+$numbers = ["alan", "sophie" , "bernard", "jacques"];
+
+// Trouvez à la plus longue cha$înede caractère
+// echo strlen("bonjour");
